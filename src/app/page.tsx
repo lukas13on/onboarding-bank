c44dbc12a3ed8e78274d15273081c511231f5add
Lucas O. Neitzke <lucas.neitzke@alunos.bpkedu.com.br>
2024-06-24 17:08:43 -0300

    cover e seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 import Application from "@/components/application/Application";
 import HomeView from "@/views/home/HomeView";
 
-import type { Metadata } from 'next'
- 
+import type { Metadata } from "next";
+
+const serverURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://onboarding-bank.vercel.app";
+
+const siteName = "Biobank";
+const title = "Biobank - Banco digital do Biopark";
+const description = "Boas vindas ao banco digital do Biopark!";
+
 export const metadata: Metadata = {
-  title: 'Biobank - Banco digital do Biopark',
-  description: 'Boas vindas ao banco digital do Biopark!',
-}
- 
+  title,
+  description: "Boas vindas ao banco digital do Biopark!",
+  openGraph: {
+    type: "website",
+    url: serverURL,
+    title: title,
+    description,
+    siteName,
+    images: [
+      {
+        url: `${serverURL}/cover.png`,
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <Application>

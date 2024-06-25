e0a1dcf664c41477de4a139a34931bd18a866fff
Lucas O. Neitzke <lucas.neitzke@alunos.bpkedu.com.br>
2024-06-25 17:56:15 -0300

    latest

--- src/services/prospect/ProspectService.ts
+++ src/services/prospect/ProspectService.ts
@@ -2,6 +2,7 @@ import ProspectModel, {
   Prospect,
   ProspectWithoutId,
 } from "@/models/prospect/ProspectModel";
+import AccountService from "../account/AccountService";
 
 /** Serviço de prospectos */
 export default class prospectService {
@@ -9,43 +10,64 @@ export default class prospectService {
    * Retorna todos os prospectos
    */
   static async getAll() {
-    return ProspectModel.getAll();
+    return await ProspectModel.getAll();
   }
 
   /**
    * Retorna um prospecto pelo id
    */
-  static getById(id_prospecto: number) {
-    return ProspectModel.getById(id_prospecto);
+  static async getById(id_prospecto: number) {
+    return await ProspectModel.getById(id_prospecto);
   }
 
   /**
    * Retorna um prospecto pelo email
    */
-  static getByEmail(email: string) {
-    return ProspectModel.getByEmail(email);
+  static async getByEmail(email: string) {
+    return await ProspectModel.getByEmail(email);
   }
 
   /**
    * Cria um prospecto
    */
-  static create(prospecto: ProspectWithoutId) {
+  static async create(prospecto: ProspectWithoutId) {
     this.validate(prospecto);
-    return ProspectModel.create(prospecto);
+    return await ProspectModel.create(prospecto);
   }
 
   /**
    * Atualiza um prospecto
    */
-  static update(id_prospecto: number, prospecto: Prospect) {
-    return ProspectModel.update(id_prospecto, prospecto);
+  static async update(id_prospecto: number, prospecto: Prospect) {
+    if (prospecto.status === true) {
+      const account = await AccountService.create({
+        nome: prospecto.nome,
+        cpf: prospecto.documento,
+        email: prospecto.email,
+        data_de_nascimento: prospecto.data_nascimento.toString(),
+        telefone: prospecto.telefone,
+        senha: prospecto.senha,
+      });
+      if (account.success) {
+        return await ProspectModel.update(id_prospecto, prospecto);
+      } else {
+        throw new Error(
+          `Não foi possível criar a conta do prospecto ${account.message}`
+        );
+      }
+    } else {
+      return await ProspectModel.update(id_prospecto, {
+        ...prospecto,
+        status: false,
+      });
+    }
   }
 
   /**
    * Deleta um prospecto
    */
-  static delete(id_prospecto: number) {
-    return ProspectModel.delete(id_prospecto);
+  static async delete(id_prospecto: number) {
+    return await ProspectModel.delete(id_prospecto);
   }
 
   static validate(prospecto: ProspectWithoutId) {
